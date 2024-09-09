372e44d7993d22b4455336854cfc29aba4449f64
slet0vr <egorpetrovskij444@gmail.com>
2024-09-09 17:18:52 +0300

    Update layout_quizz.py

--- layout_quizz.py
+++ layout_quizz.py
@@ -5,7 +5,29 @@ btn_rest = QPushButton("Відпочити")
 spin = QSpinBox()
 spin.setValue(30)
 
+def create_card_layout():
+    
+    layout = QVBoxLayout()
 
+    
+    question_label = QLabel("Яблуко")
+    layout.addWidget(question_label)
+
+    
+    radio_group_box = QGroupBox("Варіанти відповіді:")
+
+    
+    line1_quizz = QRadioButton("caterpillar")
+    line2_quizz = QRadioButton("apple")
+    line3_quizz = QRadioButton("aplication")
+    line4_quizz = QRadioButton("building")
+
+    
+    radio_button_group = QButtonGroup()
+    radio_button_group.addButton(line1_quizz)
+    radio_button_group.addButton(line2_quizz)
+    radio_button_group.addButton(line3_quizz)
+    radio_button_group.addButton(line4_quizz)
 
 
 main_line_quizz = QVBoxLayout()
